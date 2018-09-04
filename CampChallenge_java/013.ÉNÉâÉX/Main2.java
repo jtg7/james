1db5a2b23bafd3add8f8e0b4023dac33adeb7e35
james <orl.jgrimm@gmail.com>
2018-09-04 15:16:09 +0900

    クラスの継承

---
+++ CampChallenge_java/013.ÉNÉâÉX/Main2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author jamesgrimm
+ */
+public class Main2 {
+   public static void main(String[] args){
+       class2 profile2 = new class2();
+       profile2.setProfile("石見",19,"川崎市");
+       profile2.printProfile();
+       profile2.clearProfile();
+       profile2.printProfile();
+   } 
+}
---
+++ CampChallenge_java/013.ÉNÉâÉX/class2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author jamesgrimm
+ */
+public class class2 extends class1{
+    public void clearProfile(){
+        this.name=null;
+        this.age=0;
+        this.address=null;
+    }
+    
+}
