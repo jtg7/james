0e540cd3b5a3995dcc75de087b2074c582e90ae8
james <orl.jgrimm@gmail.com>
2018-09-04 15:17:37 +0900

    クラスの利用

---
+++ CampChallenge_java/013.ÉNÉâÉX/Main1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author jamesgrimm
+ */
+public class Main1{
+    
+    public static void main(String[] args){
+        
+        class1 profile = new class1();
+        profile.setProfile("石見",19,"川崎市"); 
+        profile.printProfile();
+        }
+    
+}
