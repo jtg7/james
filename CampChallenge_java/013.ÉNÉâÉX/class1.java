29facc9d174dea9fee614c441a26efef4a7bff8a
james <orl.jgrimm@gmail.com>
2018-09-04 14:27:41 +0900

    クラスの利用

---
+++ CampChallenge_java/013.ÉNÉâÉX/class1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author jamesgrimm
+ */
+public class class1 {
+   public String name = "";
+public int age = 0;
+public String address = "";
+
+public void setProfile(String n,int a,String ad){   
+this.name=n;
+this.age=a;
+this.address=ad;
+}
+public void printProfile(){
+System.out.print(name+age+address);
+
+}
+} 
+
