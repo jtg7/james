a5a2da61f606943fe52708b47987acbd2d17ec16
james <orl.jgrimm@gmail.com>
2018-09-18 13:36:11 +0900

    タイムスタンプ

---
+++ CampChallenge_java/015.ïWèÄÉNÉâÉX/Hizuke.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hizuke;
+import java.util.Date;
+/**
+ *
+ * @author jamesgrimm
+ */
+public class Hizuke {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Date d = new Date(1451574000000L);
+      System.out.print(d);
+    }
+    
+}
