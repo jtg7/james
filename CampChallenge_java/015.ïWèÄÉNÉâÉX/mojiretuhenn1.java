4718998b2fbdd7e5a2b5d9893fdb2ab4bb192506
james <orl.jgrimm@gmail.com>
2018-09-19 13:44:27 +0900

    バイト数と文字数の表示

---
+++ CampChallenge_java/015.ïWèÄÉNÉâÉX/mojiretuhenn1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jamesgrimm
+ */
+public class mojiretuhenn1 {
+    public static void main(String[] args){
+        String name = "いわみ";
+        System.out.println(name.length());
+        System.out.print(name.getBytes().length);
+        
+    }
+
+}
